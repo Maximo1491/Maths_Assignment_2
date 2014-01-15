aa90aa8e17158d075bf538b59cbfc68224f6c68d
Maximo1491 <maximo1491@hotmail.com>
2014-01-15 18:21:52 +0000

    Perlin Noise mother fuckers

--- src/platform/platform.h
+++ src/platform/platform.h
@@ -151,9 +151,9 @@ static char *get_sprintf_buffer() {
 #define CX 16
 #define CY 16
 #define CZ 16
-#define SCX 8
+#define SCX 16
 #define SCY 8
-#define SCZ 8
+#define SCZ 16
 #include "../math/chunk.h"
 #include "../math/super_chunk.h"
 
